8fbfc015f4fba33920f7be0c753b57150bb7457d
June <june@peanut.one>
2024-01-01 22:40:51 -0500

    migrate from argparse to virtualenv
    This move comes with a couple reductions in parsing functionality that I think are more than worth the simpler program flow.
    
    `update` no longer accepts an infinite set of pairs of field and value. Each command invocation will perform one column update on one single episode. This is fine.
    
    Removed the ability to use multiple IDs for all functions, though this shouldn't be difficult to add back at all.
    
    I will have to readd command completion at a later date.

--- snapcast_admin/__init__.py
+++ snapcast_admin/__init__.py
@@ -5,26 +5,20 @@
 This script does not ask for confirmation and does not back up data before
 destructive commands. Be careful.
 """
-from argparse import (
-    Action,
-    ArgumentError,
-    ArgumentParser,
-    ArgumentTypeError,
-    Namespace,
-)
+
 from dataclasses import KW_ONLY, dataclass
 from datetime import datetime, timedelta, timezone
-from pprint import pprint as pp
+from pprint import pformat as pf
 from shutil import get_terminal_size
-from typing import Any, Iterable, Sequence
+from typing import Iterable, Optional
 from urllib.parse import unquote
 from uuid import UUID
 
-from argcomplete import autocomplete, safe_actions  # type:ignore
-from argcomplete.completers import SuppressCompleter
+import click
+import requests
 from b2sdk.exception import FileNotPresent
 from b2sdk.v2 import B2Api, Bucket, InMemoryAccountInfo
-from requests import delete, get, patch
+from click import Context, Parameter
 from wcwidth import wcwidth
 
 database_fields = (
@@ -38,41 +32,6 @@ BEARER_TOKEN = {'Authorization': "Bearer 628c17c9-f2b8-4616-a5fb-f4a9759f32c9"}
 BASE_URL = "https://www.peanut.one/snapcast"
 # BASE_URL = "http://192.168.1.176:5000/snapcast"
 FEED_ID = "1787bd99-9d00-48c3-b763-5837f8652bd9"
-
-
-class FieldValueAction(Action):
-    """Validate and pair off db fields and new values.
-
-    ['title', 'Python 3', 'subtitle', 'The Return'] ->
-        [['title', 'Python 3], ['subtitle', 'The Return']]
-
-    ['media_duration', '999', 'pub_date'] ->
-        ArgumentError, namespace now contains `fv_partial=True`
-
-    ['titl', 'Python 3'] -> ArgumentError
-
-    If validation passes, the result list is saved to the Namespace.
-    """
-
-    def __call__(self,  # noqa D103
-                 parser: ArgumentParser,
-                 namespace: Namespace,
-                 values: str | Sequence[Any] | None,
-                 option_string: str | None = None,
-                 ) -> None:
-        if not values:
-            raise ArgumentError(self, "Arguments not present.")
-
-        if len(values) % 2 != 0:
-            namespace.fv_partial = True
-            raise ArgumentError(self, "Mismatched field value pair")
-
-        pairs = [list(i) for i in zip(*[iter(values)] * 2)]
-        if any(pair[0] not in database_fields for pair in pairs):
-            raise ArgumentError(
-                self, f"field must be one of "f"[{", ".join(database_fields)}]")
-
-        setattr(namespace, self.dest, pairs)
 
 
 @dataclass
@@ -101,38 +60,32 @@ class Episode:  # noqa: D101
             self.pub_date = datetime.fromisoformat(pd)
 
 
-# noinspection PyUnusedLocal
-def paired_field_value_completer(
-        parsed_args: Namespace, **kwargs: str) -> Iterable[str]:
-    """Alternates completion of fields and the values to update.
+class EpisodeIdType(click.ParamType):  # noqa: D101
+    name = "episode id"
 
-    If the `FieldValueAction` has the column but not the new value, it leaves
-    us a flag in the namespace we can check for. If found, don't complete.
-    """
-    if hasattr(parsed_args, 'fv_partial'):
-        return []
-    return database_fields
+    def convert(
+            self, value: str,
+            param: Optional[Parameter],
+            ctx: Optional[Context],
+    ) -> str:
+        """Validate command line episode id or comma-separated list of same.
 
-
-def episode_id_converter(s: str) -> Iterable[str]:
-    """Validate command line episode id or comma-separated list of same.
-
-    :param s: The argument from the command line
-    :raises ArgumentTypeError: if any id is invalid.
-    :return: A list of episode ids.
-    """
-    items = s.split(",")
-    for item in items:
+        :param value: The argument from the command line
+        :param param: uhhh.
+        :param ctx: uhh
+        :raises ArgumentTypeError: if any id is invalid.
+        :return: A list of episode ids.
+        """
         try:
-            item_id = int(item)
+            item_id = int(value)
             if not (item_id > 0 or item_id == -1):  # Must be positive or -1
-                raise ArgumentTypeError(f"{item_id} is not a valid episode ID")
+                self.fail(f"{item_id} is not a valid episode ID")
         except ValueError:
             try:
-                UUID(item)
+                UUID(value)
             except ValueError:
-                raise ArgumentTypeError(f"{item} is not a valid uuid") from None
-    return items
+                self.fail(f"{value} is not a valid uuid")
+        return value
 
 
 def fwtruncate(s: str, max_width: int, min_width: int = 0) -> str:
@@ -169,7 +122,7 @@ def get_b2() -> tuple[B2Api, Bucket]:
 
 def get_all_episodes() -> Iterable[Episode]:
     """Retrieve all episodes from the server."""
-    data = get(
+    data = requests.get(
         f"{BASE_URL}/{FEED_ID}/episodes",
         headers=BEARER_TOKEN,
     )
@@ -178,7 +131,7 @@ def get_all_episodes() -> Iterable[Episode]:
     return [Episode(**i) for i in data.json()]
 
 
-def episode_info(episode_id: str | int) -> Episode:
+def episode_info(episode_id: str | int) -> Optional[Episode]:
     """Retrieve all data for the specified episode.
 
     :param episode_id: The ID of the episode to retrieve information for.
@@ -187,22 +140,36 @@ def episode_info(episode_id: str | int) -> Episode:
     :raises ArgumentError: if the ``episode_id`` turns out to be invalid.
     :return: An `Episode` containing data for the ``episode_id``
     """
-    response = get(f"{BASE_URL}/{FEED_ID}/episode/{episode_id}")
+    response = requests.get(f"{BASE_URL}/{FEED_ID}/episode/{episode_id}")
     if response.status_code == 404:
-        raise ArgumentError(None, f"Episode {episode_id} was not found")
+        raise click.BadParameter(f"Episode {episode_id} was not found")
     return Episode(**response.json())
 
 
-def handle_list(args: Namespace) -> None:
-    """Process the "list" subcommand from the CLI."""
+@click.group(context_settings={"max_content_width": 90})
+def cli() -> None:
+    """Remote control script for the snapcast podcast host."""
+    pass
+
+
+@cli.command(name="list")
+@click.option(
+    "--sort",
+    help="sort results. default: pub_date",
+    default="pub_date",
+    type=click.Choice(['pub_date', 'id']),
+)
+@click.option("--find", help="filter output to results containing TEXT")
+def list_(sort: str, find: Optional[str]) -> None:
+    """Print a list of all episodes."""
     episodes = get_all_episodes()
 
-    if args.find:
-        episodes = filter(lambda x: args.find in x.title, episodes)
+    if find:
+        episodes = filter(lambda x: find in x.title, episodes)
 
     lines = []
     width = get_terminal_size((0, 0)).columns
-    for ep in sorted(episodes, key=lambda x: getattr(x, args.sort)):
+    for ep in sorted(episodes, key=lambda x: getattr(x, sort)):
         lines.append("│ ".join([
             f"{ep.id:3}",
             f"{ep.pub_date:%Y-%m-%d}",
@@ -210,42 +177,53 @@ def handle_list(args: Namespace) -> None:
             f"{ep.media_size / 1000000:#6.2F} MB",
             f"{fwtruncate(ep.title, width - 38)}" if width else f"{ep.title}",
         ]))
-    print(*lines, sep="\n")
+    click.echo("\n".join(lines))
 
 
-def handle_info(args: Namespace) -> None:
-    """Process the "info" subcommand from the CLI."""
-    pp(episode_info(args.id[0]))
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("episode_id", required=True, type=EpisodeIdType())
+def info(episode_id: str) -> None:
+    """Fetch information about an episode."""
+    if (episode := episode_info(episode_id)) is None:
+        raise click.BadParameter(f"ID {episode_id} not found.")
+    click.echo(pf(episode))
 
 
-def handle_update(args: Namespace) -> None:
-    """Process the "update" subcommand from the CLI."""
-    episode_uuids = [episode_info(i).uuid for i in args.id]
+@cli.command(context_settings={'ignore_unknown_options': True})
+@click.argument("episode_id", required=True, type=EpisodeIdType())
+@click.argument("field", required=True, type=click.Choice(database_fields))
+@click.argument("value", required=True)
+def update(episode_id: str, field: str, value: str) -> None:
+    """Update episode info in the remote host."""
+    if (episode := episode_info(episode_id)) is None:
+        raise click.BadParameter(f"ID {episode_id} not found.")
+    episode_uuid = episode.uuid
 
-    for pair in args.set:
-        match pair[0]:
-            case "media_duration":
-                # Convert [[HH:]MM:]SS to integer seconds.
-                pair[1] = sum(60 ** i * int(v) for i, v in
-                              enumerate(pair[1].split(":")[::-1]))
-            case "pub_date":
-                # Convert local "YYYY-MM-DD[ HH:MM]" to full spec UTC string.
-                d = datetime.fromisoformat(pair[1])
-                pair[1] = d.astimezone(timezone.utc).isoformat()
+    match field:
+        case "media_duration":
+            # Convert [[HH:]MM:]SS to integer seconds.
+            value = sum(60 ** i * int(v) for i, v in
+                        enumerate(value.split(":")[::-1]))
+        case "pub_date":
+            # Convert a local-tz "YYYY-MM-DD[ HH:MM]" to full spec UTC string.
+            dt = datetime.fromisoformat(value)
+            value = dt.astimezone(timezone.utc).isoformat()
 
-    for episode_uuid in episode_uuids:
-        patch(
-            f"{BASE_URL}/{FEED_ID}/episode/{episode_uuid}",
-            json=dict(args.set),
-            headers=BEARER_TOKEN,
-        )
+    requests.patch(
+        f"{BASE_URL}/{FEED_ID}/episode/{episode_uuid}",
+        headers=BEARER_TOKEN,
+        json={field: value},
+    )
 
 
-def handle_delete(args: Namespace) -> None:
-    """Process the "delete" subcommand from the CLI."""
-    episode = episode_info(args.id[0])
+@cli.command()
+@click.argument("episode_id", required=True, type=EpisodeIdType())
+def delete(episode_id: str) -> None:
+    """Delete episodes."""
+    if (episode := episode_info(episode_id)) is None:
+        raise click.BadParameter(f"ID {episode_id} not found.")
 
-    delete(
+    requests.delete(
         f"{BASE_URL}/{FEED_ID}/episode/{episode.uuid}",
         headers=BEARER_TOKEN,
     )
@@ -259,89 +237,8 @@ def handle_delete(args: Namespace) -> None:
         except FileNotPresent:
             break
 
-    print(f"Episode entry #{args.id[0]} successfuly deleted")
-
-
-def run() -> None:
-    """Script entry point."""
-    admin_parser = ArgumentParser()
-    subparsers = admin_parser.add_subparsers()
-
-    # -------- LIST --------------------
-    list_parser = subparsers.add_parser(
-        "list",
-        help="list all episodes",
-    )
-    list_parser.add_argument(
-        "--sort",
-        help="sort results. default: %(default)s",
-        choices=('pub_date', 'id'),
-        default='pub_date',
-    )
-    list_parser.add_argument(
-        "--find",
-        help="filter output to results containing FIND",
-    )
-
-    # -------- INFO --------------------
-    info_parser = subparsers.add_parser(
-        "info",
-        help="get episode information",
-    )
-    info_parser.add_argument(  # type: ignore
-        "id",
-        help="id or uuid of the episode to fetch, or -1 for most recent",
-        type=episode_id_converter,
-    ).completer = SuppressCompleter
-
-    # -------- UPDATE --------------------
-    update_parser = subparsers.add_parser(
-        "update",
-        help="update a field of an episode",
-        usage="%(prog)s id[,id...] field value [field value...]",
-    )
-    update_parser.add_argument(  # type: ignore
-        "id",
-        help="id or uuid of the episode to update",
-        type=episode_id_converter,
-    ).completer = SuppressCompleter
-    update_parser.add_argument(  # type: ignore
-        "set",
-        help="database column and new value pair",
-        action=FieldValueAction,
-        metavar="<field value>",
-        nargs='*',
-    ).completer = paired_field_value_completer
-
-    # -------- DELETE --------------------
-    delete_parser = subparsers.add_parser(
-        "delete",
-        help="delete episodes",
-    )
-    delete_parser.add_argument(  # type: ignore
-        "id",
-        help="id or uuid of the episode to delete",
-        type=episode_id_converter,
-    ).completer = SuppressCompleter
-
-    list_parser.set_defaults(func=handle_list)
-    info_parser.set_defaults(func=handle_info)
-    update_parser.set_defaults(func=handle_update)
-    delete_parser.set_defaults(func=handle_delete)
-
-    # This is a lie, we absolutely have side-effects. It's necessary for the
-    # completer to work though as the errors we'd want to throw on incorrect
-    # input during normal execution would leave the completer with no context.
-    safe_actions.add(FieldValueAction)
-    autocomplete(admin_parser, exclude=["-h", "--help"])
-    arguments = admin_parser.parse_args()
-
-    if hasattr(arguments, "func"):
-        try:
-            arguments.func(arguments)
-        except ArgumentError as err:
-            print(err)
+    click.echo(f"Episode entry #{episode_id} successfuly deleted")
 
 
 if __name__ == '__main__':
-    run()
+    cli()
