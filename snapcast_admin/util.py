f83fa0a516900c06cd1833589e405349870eeca3
June <june@peanut.one>
2024-01-09 23:40:37 -0500

    Refactor.

--- snapcast_admin/util.py
+++ snapcast_admin/util.py
@@ -5,14 +5,15 @@ from b2sdk.api import B2Api
 from b2sdk.bucket import Bucket
 from wcwidth import wcwidth
 
-B2_APP_KEY_ID = environ['SNADMIN_B2_APP_KEY_ID']
-B2_APP_KEY = environ['SNADMIN_B2_APP_KEY']
-
 
 def get_b2() -> tuple[B2Api, Bucket]:
     """Return B2Api object and the `jbc-external` bucket."""
     b2 = B2Api(InMemoryAccountInfo())
-    b2.authorize_account("production", B2_APP_KEY_ID, B2_APP_KEY)
+    b2.authorize_account(
+        "production",
+        environ['SNADMIN_B2_APP_KEY_ID'],
+        environ['SNADMIN_B2_APP_KEY_ID'],
+    )
     bucket = b2.get_bucket_by_name("jbc-external")
     return b2, bucket
 
